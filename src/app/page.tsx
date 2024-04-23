3758b9d5ff87e8f8c1d55724680cc15f911e1d50
Sam Marshall <sam.marshall@v2.digital>
2024-04-23 16:39:50 +1000

    add shadcn/ui to the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,37 @@
+import { CreatePost } from "~/components/create-post";
+import { DataTable } from "~/components/data-table";
 import Link from "next/link";
-
-import { CreatePost } from "~/app/_components/create-post";
-import { getServerAuthSession } from "~/server/auth";
+import { UserNav } from "~/components/user-nav";
 import { api } from "~/trpc/server";
+import { columns } from "~/components/data-table/columns";
+import { promises as fs } from "fs";
+import { getServerAuthSession } from "~/server/auth";
+import path from "path";
+import { taskSchema } from "~/server/data/schema";
+import { z } from "zod";
+
+// Simulate a database read for tasks.
+async function getTasks() {
+  const data = await fs.readFile(
+    path.join(process.cwd(), "src/server/data/tasks.json"),
+  );
+
+  const tasks = JSON.parse(data.toString());
+
+  return z.array(taskSchema).parse(tasks);
+}
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
   const session = await getServerAuthSession();
+  const tasks = await getTasks();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+          rekurve
         </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
         <div className="flex flex-col items-center gap-2">
           <p className="text-2xl text-white">
             {hello ? hello.greeting : "Loading tRPC query..."}
@@ -57,6 +51,23 @@ export default async function Home() {
         </div>
 
         <CrudShowcase />
+
+        <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+          <div className="flex items-center justify-between space-y-2">
+            <div>
+              <h2 className="text-2xl font-bold tracking-tight">
+                Welcome back!
+              </h2>
+              <p className="text-muted-foreground">
+                Here&apos;s a list of your tasks for this month!
+              </p>
+            </div>
+            <div className="flex items-center space-x-2">
+              <UserNav />
+            </div>
+          </div>
+          <DataTable data={tasks} columns={columns} />
+        </div>
       </div>
     </main>
   );

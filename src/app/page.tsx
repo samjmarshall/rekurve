10ead706e40370816dd9a84ba51f79ac12210d0f
Sam Marshall <sam.marshall@v2.digital>
2024-04-27 02:43:20 +1000

    fix: scroll area for project overview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -111,310 +111,202 @@ export default function Home() {
     //     <DataTable data={tasks} columns={columns} />
     //   </div>
     // </div>
-    <div className="flex h-full">
+    <div className="flex h-full flex-col lg:flex-row">
       <SidebarNav
-        className="w-64 bg-white px-4 py-5 drop-shadow"
+        className="w-64 min-w-full overflow-x-scroll px-4 py-5 drop-shadow lg:min-w-fit lg:bg-white"
         items={sidebarNavItems}
       />
-      <div className="flex flex-1 flex-col overflow-hidden bg-[#edf2f7]">
-        <div className="flex flex-1 overflow-hidden">
-          <div className="flex w-3/4 flex-col space-y-4 px-4 py-5">
-            <Card className="flex-1">
-              <CardHeader>
-                <CardTitle>Financial Chart</CardTitle>
-              </CardHeader>
-              <CardContent className="flex justify-center p-4">
-                <LabelledpieChart className="h-[300px] w-1/2" />
-              </CardContent>
-            </Card>
-            <Card className="flex-1">
-              <CardHeader>
-                <CardTitle>Action Items</CardTitle>
-              </CardHeader>
-              <CardContent className="p-4">
-                <p>There are no overdue items to show</p>
-              </CardContent>
-            </Card>
-            <Card className="flex-1">
-              <CardHeader>
-                <CardTitle>Schedule Progress</CardTitle>
-              </CardHeader>
-              <CardContent className="p-4">
-                <Table>
-                  <TableHeader>
-                    <TableRow>
-                      <TableHead>Name</TableHead>
-                      <TableHead>Start Date</TableHead>
-                      <TableHead>End Date</TableHead>
-                      <TableHead>Completion %</TableHead>
-                    </TableRow>
-                  </TableHeader>
-                  <TableBody>
-                    <TableRow>
-                      <TableCell>1st Frame Delivery (am)</TableCell>
-                      <TableCell>21/06/2024</TableCell>
-                      <TableCell>21/06/2024</TableCell>
-                      <TableCell>0%</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>Lower Frame</TableCell>
-                      <TableCell>21/06/2024</TableCell>
-                      <TableCell>27/06/2024</TableCell>
-                      <TableCell>40%</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>Floor Joists</TableCell>
-                      <TableCell>28/06/2024</TableCell>
-                      <TableCell>02/07/2024</TableCell>
-                      <TableCell>0%</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>Structure Floor</TableCell>
-                      <TableCell>03/07/2024</TableCell>
-                      <TableCell>04/07/2024</TableCell>
-                      <TableCell>0%</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>Upper Frame</TableCell>
-                      <TableCell>05/07/2024</TableCell>
-                      <TableCell>11/07/2024</TableCell>
-                      <TableCell>0%</TableCell>
-                    </TableRow>
-                  </TableBody>
-                </Table>
-              </CardContent>
-            </Card>
-          </div>
-          <div className="flex w-1/4 flex-col space-y-4 px-4 py-5">
-            <Card className="flex-1">
-              <CardHeader>
-                <CardTitle>Finance</CardTitle>
-              </CardHeader>
-              <CardContent className="p-4">
-                <Table>
-                  <TableHeader>
-                    <TableRow>
-                      <TableHead>Client invoices</TableHead>
-                      <TableHead>Paid</TableHead>
-                      <TableHead>Total</TableHead>
-                    </TableRow>
-                  </TableHeader>
-                  <TableBody>
-                    <TableRow>
-                      <TableCell />
-                      <TableCell>$0.00</TableCell>
-                      <TableCell>$0.00</TableCell>
-                    </TableRow>
-                  </TableBody>
-                </Table>
-                <Table>
-                  <TableHeader>
-                    <TableRow>
-                      <TableHead>Bills</TableHead>
-                      <TableHead>Paid</TableHead>
-                      <TableHead>Total</TableHead>
-                    </TableRow>
-                  </TableHeader>
-                  <TableBody>
-                    <TableRow>
-                      <TableCell />
-                      <TableCell>$16758.00</TableCell>
-                      <TableCell>$16758.00</TableCell>
-                    </TableRow>
-                  </TableBody>
-                </Table>
-                <Table>
-                  <TableHeader>
-                    <TableRow>
-                      <TableHead>Variations</TableHead>
-                      <TableHead>Approved</TableHead>
-                      <TableHead>Total</TableHead>
-                    </TableRow>
-                  </TableHeader>
-                  <TableBody>
-                    <TableRow>
-                      <TableCell />
-                      <TableCell>$0.00</TableCell>
-                      <TableCell>$0.00</TableCell>
-                    </TableRow>
-                  </TableBody>
-                </Table>
-                <Table>
-                  <TableHeader>
-                    <TableRow>
-                      <TableHead>Purchase orders</TableHead>
-                      <TableHead>Approved</TableHead>
-                      <TableHead>Total</TableHead>
-                    </TableRow>
-                  </TableHeader>
-                  <TableBody>
-                    <TableRow>
-                      <TableCell />
-                      <TableCell>$94058.00</TableCell>
-                      <TableCell>$94058.00</TableCell>
-                    </TableRow>
-                  </TableBody>
-                </Table>
-                <Link className="mt-2 block text-blue-600" href="#">
-                  See more
-                </Link>
-              </CardContent>
-            </Card>
-            <Card className="flex-1">
-              <CardHeader>
-                <CardTitle>Tasks</CardTitle>
-              </CardHeader>
-              <CardContent className="p-4">
-                <p>There are no tasks to show</p>
-                <Button className="mt-4" variant="outline">
-                  Add Task
-                </Button>
-              </CardContent>
-            </Card>
-            <Card className="flex-1">
-              <CardHeader>
-                <CardTitle>Recent Files</CardTitle>
-              </CardHeader>
-              <CardContent className="p-4">
-                <ScrollArea className="space-y-2">
-                  <img
-                    alt="File 1"
-                    className="h-24 w-24"
-                    height="100"
-                    src="/placeholder.svg"
-                    style={{
-                      aspectRatio: "100/100",
-                      objectFit: "cover",
-                    }}
-                    width="100"
-                  />
-                  <img
-                    alt="File 2"
-                    className="h-24 w-24"
-                    height="100"
-                    src="/placeholder.svg"
-                    style={{
-                      aspectRatio: "100/100",
-                      objectFit: "cover",
-                    }}
-                    width="100"
-                  />
-                  <img
-                    alt="File 3"
-                    className="h-24 w-24"
-                    height="100"
-                    src="/placeholder.svg"
-                    style={{
-                      aspectRatio: "100/100",
-                      objectFit: "cover",
-                    }}
-                    width="100"
-                  />
-                  <img
-                    alt="File 4"
-                    className="h-24 w-24"
-                    height="100"
-                    src="/placeholder.svg"
-                    style={{
-                      aspectRatio: "100/100",
-                      objectFit: "cover",
-                    }}
-                    width="100"
-                  />
-                  <img
-                    alt="File 5"
-                    className="h-24 w-24"
-                    height="100"
-                    src="/placeholder.svg"
-                    style={{
-                      aspectRatio: "100/100",
-                      objectFit: "cover",
-                    }}
-                    width="100"
-                  />
-                  <img
-                    alt="File 6"
-                    className="h-24 w-24"
-                    height="100"
-                    src="/placeholder.svg"
-                    style={{
-                      aspectRatio: "100/100",
-                      objectFit: "cover",
-                    }}
-                    width="100"
-                  />
-                  <img
-                    alt="File 7"
-                    className="h-24 w-24"
-                    height="100"
-                    src="/placeholder.svg"
-                    style={{
-                      aspectRatio: "100/100",
-                      objectFit: "cover",
-                    }}
-                    width="100"
-                  />
-                  <img
-                    alt="File 8"
-                    className="h-24 w-24"
-                    height="100"
-                    src="/placeholder.svg"
-                    style={{
-                      aspectRatio: "100/100",
-                      objectFit: "cover",
-                    }}
-                    width="100"
-                  />
-                  <img
-                    alt="File 9"
-                    className="h-24 w-24"
-                    height="100"
-                    src="/placeholder.svg"
-                    style={{
-                      aspectRatio: "100/100",
-                      objectFit: "cover",
-                    }}
-                    width="100"
-                  />
-                  <img
-                    alt="File 10"
-                    className="h-24 w-24"
-                    height="100"
-                    src="/placeholder.svg"
-                    style={{
-                      aspectRatio: "100/100",
-                      objectFit: "cover",
-                    }}
-                    width="100"
-                  />
-                  <img
-                    alt="File 11"
-                    className="h-24 w-24"
-                    height="100"
-                    src="/placeholder.svg"
-                    style={{
-                      aspectRatio: "100/100",
-                      objectFit: "cover",
-                    }}
-                    width="100"
-                  />
-                  <img
-                    alt="File 12"
-                    className="h-24 w-24"
-                    height="100"
-                    src="/placeholder.svg"
-                    style={{
-                      aspectRatio: "100/100",
-                      objectFit: "cover",
-                    }}
-                    width="100"
-                  />
-                </ScrollArea>
-              </CardContent>
-            </Card>
+      <ScrollArea className="w-full bg-[#edf2f7]">
+        <div className="flex flex-1 flex-col">
+          <div className="flex flex-1">
+            <div className="flex w-3/4 flex-col space-y-4 px-4 py-5">
+              <Card className="flex-1">
+                <CardHeader>
+                  <CardTitle>Financial Chart</CardTitle>
+                </CardHeader>
+                <CardContent className="flex justify-center p-4">
+                  <LabelledpieChart className="h-[300px] w-1/2" />
+                </CardContent>
+              </Card>
+              <Card className="flex-1">
+                <CardHeader>
+                  <CardTitle>Action Items</CardTitle>
+                </CardHeader>
+                <CardContent className="p-4">
+                  <p>There are no overdue items to show</p>
+                </CardContent>
+              </Card>
+              <Card className="flex-1">
+                <CardHeader>
+                  <CardTitle>Schedule Progress</CardTitle>
+                </CardHeader>
+                <CardContent className="p-4">
+                  <Table>
+                    <TableHeader>
+                      <TableRow>
+                        <TableHead>Name</TableHead>
+                        <TableHead>Start Date</TableHead>
+                        <TableHead>End Date</TableHead>
+                        <TableHead>Completion %</TableHead>
+                      </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                      <TableRow>
+                        <TableCell>1st Frame Delivery (am)</TableCell>
+                        <TableCell>21/06/2024</TableCell>
+                        <TableCell>21/06/2024</TableCell>
+                        <TableCell>0%</TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell>Lower Frame</TableCell>
+                        <TableCell>21/06/2024</TableCell>
+                        <TableCell>27/06/2024</TableCell>
+                        <TableCell>40%</TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell>Floor Joists</TableCell>
+                        <TableCell>28/06/2024</TableCell>
+                        <TableCell>02/07/2024</TableCell>
+                        <TableCell>0%</TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell>Structure Floor</TableCell>
+                        <TableCell>03/07/2024</TableCell>
+                        <TableCell>04/07/2024</TableCell>
+                        <TableCell>0%</TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell>Upper Frame</TableCell>
+                        <TableCell>05/07/2024</TableCell>
+                        <TableCell>11/07/2024</TableCell>
+                        <TableCell>0%</TableCell>
+                      </TableRow>
+                    </TableBody>
+                  </Table>
+                </CardContent>
+              </Card>
+            </div>
+            <div className="flex w-1/4 flex-col space-y-4 px-4 py-5">
+              <Card className="flex-1">
+                <CardHeader>
+                  <CardTitle>Finance</CardTitle>
+                </CardHeader>
+                <CardContent className="p-4">
+                  <Table>
+                    <TableHeader>
+                      <TableRow>
+                        <TableHead>Client invoices</TableHead>
+                        <TableHead>Paid</TableHead>
+                        <TableHead>Total</TableHead>
+                      </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                      <TableRow>
+                        <TableCell />
+                        <TableCell>$0.00</TableCell>
+                        <TableCell>$0.00</TableCell>
+                      </TableRow>
+                    </TableBody>
+                  </Table>
+                  <Table>
+                    <TableHeader>
+                      <TableRow>
+                        <TableHead>Bills</TableHead>
+                        <TableHead>Paid</TableHead>
+                        <TableHead>Total</TableHead>
+                      </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                      <TableRow>
+                        <TableCell />
+                        <TableCell>$16758.00</TableCell>
+                        <TableCell>$16758.00</TableCell>
+                      </TableRow>
+                    </TableBody>
+                  </Table>
+                  <Table>
+                    <TableHeader>
+                      <TableRow>
+                        <TableHead>Variations</TableHead>
+                        <TableHead>Approved</TableHead>
+                        <TableHead>Total</TableHead>
+                      </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                      <TableRow>
+                        <TableCell />
+                        <TableCell>$0.00</TableCell>
+                        <TableCell>$0.00</TableCell>
+                      </TableRow>
+                    </TableBody>
+                  </Table>
+                  <Table>
+                    <TableHeader>
+                      <TableRow>
+                        <TableHead>Purchase orders</TableHead>
+                        <TableHead>Approved</TableHead>
+                        <TableHead>Total</TableHead>
+                      </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                      <TableRow>
+                        <TableCell />
+                        <TableCell>$94058.00</TableCell>
+                        <TableCell>$94058.00</TableCell>
+                      </TableRow>
+                    </TableBody>
+                  </Table>
+                  <Link className="mt-2 block text-blue-600" href="#">
+                    See more
+                  </Link>
+                </CardContent>
+              </Card>
+              <Card className="flex-1">
+                <CardHeader>
+                  <CardTitle>Tasks</CardTitle>
+                </CardHeader>
+                <CardContent className="p-4">
+                  <p>There are no tasks to show</p>
+                  <Button className="mt-4" variant="outline">
+                    Add Task
+                  </Button>
+                </CardContent>
+              </Card>
+              <Card className="flex-1">
+                <CardHeader>
+                  <CardTitle>Recent Files</CardTitle>
+                </CardHeader>
+                <CardContent className="p-4">
+                  <ScrollArea className="space-y-2">
+                    <img
+                      alt="File 1"
+                      className="h-24 w-24"
+                      height="100"
+                      src="/placeholder.svg"
+                      style={{
+                        aspectRatio: "100/100",
+                        objectFit: "cover",
+                      }}
+                      width="100"
+                    />
+                    <img
+                      alt="File 2"
+                      className="h-24 w-24"
+                      height="100"
+                      src="/placeholder.svg"
+                      style={{
+                        aspectRatio: "100/100",
+                        objectFit: "cover",
+                      }}
+                      width="100"
+                    />
+                  </ScrollArea>
+                </CardContent>
+              </Card>
+            </div>
           </div>
         </div>
-      </div>
+      </ScrollArea>
     </div>
   );
 }

0b19bef0f20587747a0e204f163fed552d7adfe3
Sam Marshall <sam.marshall@v2.digital>
2024-05-24 17:15:14 +1000

    fix(login): load side panel image after width can be calculated

--- src/app/(login)/_components/side-panel.tsx
+++ src/app/(login)/_components/side-panel.tsx
@@ -1,28 +1,38 @@
 "use client"
 
+import { type SyntheticEvent, useEffect, useState } from "react"
+
 import Image from "next/image"
 import Link from "next/link"
 import { Logo } from "~/components/logo"
-import { type SyntheticEvent } from "react"
 import { useWindowSize } from "~/hooks/use-window-size"
 
 export function SidePanel() {
   const { width, height } = useWindowSize()
+  const [loadImage, setLoadImage] = useState<boolean>(false)
+
+  useEffect(() => {
+    // Ensures the value is a valid integer or float
+    if (Number.isFinite(width)) {
+      setLoadImage(true)
+    }
+  }, [width])
 
   return (
-    <div className="relative hidden h-screen flex-col overflow-hidden bg-muted p-10 text-white dark:border-r lg:flex">
+    <div className="relative hidden h-screen flex-col overflow-hidden bg-muted p-10 text-white lg:flex dark:border-r">
       <div className="absolute inset-0 bg-black" />
-      <Image
-        src="/assets/login-background.webp"
-        width={width / 2}
-        height={height}
-        alt="background image"
-        className="absolute inset-0 w-full opacity-0 transition-opacity duration-300 ease-linear"
-        onLoad={(e: SyntheticEvent) =>
-          (e.target as HTMLImageElement).classList.remove("opacity-0")
-        }
-        priority
-      />
+      {loadImage && (
+        <Image
+          src="/assets/login-background.webp"
+          width={width / 2}
+          height={height}
+          alt="background image"
+          className="absolute inset-0 w-full opacity-0 transition-opacity duration-300 ease-linear"
+          onLoad={(e: SyntheticEvent) =>
+            (e.target as HTMLImageElement).classList.remove("opacity-0")
+          }
+        />
+      )}
 
       <Link className="relative z-20 flex items-center text-3xl" href="/">
         <Logo />

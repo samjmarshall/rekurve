5a09b2fa0f8d191ed5cbef87986e7adad4828b0d
Sam Marshall <sam.marshall@v2.digital>
2024-05-28 13:18:38 +1000

    fix(env): removed unused env

--- src/env.js
+++ src/env.js
@@ -61,7 +61,6 @@ export const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    LAST_MODIFIED: process.env.LAST_MODIFIED,
     LOG_LEVEL: process.env.LOG_LEVEL,
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,

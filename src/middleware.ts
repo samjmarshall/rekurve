603611c6b57aee6c26c9bd87674e60b2ac0db52a
Sam Marshall <sam.marshall@v2.digital>
2024-06-01 12:47:34 +1000

    feat(csp): use csp nonce on protected pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { type NextRequest, NextResponse } from "next/server"
 
 import { env } from "~/env"
+import logger from "./server/logger"
 
 export const config = {
   matcher: [
@@ -23,49 +24,63 @@ export const config = {
 }
 
 export function middleware(request: NextRequest) {
-  // Allow Google Tag Manager preview and debug sources in development
-  const gtmPreviewAndDebugSources = env.NODE_ENV === "development"
+  const requestHeaders = new Headers(request.headers)
+  let cspHeader: string
 
-  // When NODE_ENV is "development", allow 'unsafe-eval' for webpack HMR
-  const cspHeader = `
-    default-src 'none';
-    base-uri 'none';
-    connect-src 'self' https://www.google-analytics.com/g/collect;
-    font-src 'self' ${gtmPreviewAndDebugSources ? "https://fonts.gstatic.com" : ""};
-    frame-src https://www.google.com/recaptcha/;
-    img-src 'self' data: https://lh3.googleusercontent.com ${gtmPreviewAndDebugSources ? "https://www.googletagmanager.com https://fonts.gstatic.com" : ""};
-    script-src 'self' ${env.NODE_ENV === "development" ? "'unsafe-eval'" : ""};
-    script-src-elem 'self' 'unsafe-inline' https://www.googletagmanager.com/gtm.js https://www.googletagmanager.com/gtag/js https://www.google.com/recaptcha/enterprise.js https://www.gstatic.com/recaptcha/releases/;
-    style-src 'self';
-    style-src-elem 'self' 'unsafe-inline' ${gtmPreviewAndDebugSources ? "https://www.googletagmanager.com https://fonts.googleapis.com" : ""};
-    upgrade-insecure-requests;
-    report-uri /api/csp-reports;
-  `
-  // TODO: Try implementing a nonce-based CSP header for (application) pages only. Leaving public/crawlable (website) & (login) pages without a nonce-based CSP header, like above. See: https://nextjs.org/docs/app/building-your-application/routing/middleware#conditional-statements
-  // CSP header when using 'nonce' - WARNING: Using 'nonce' will prevent index/page caching as each request will have a unique nonce
-  // const nonce = Buffer.from(crypto.randomUUID()).toString("base64")
-  //
-  // const cspHeader = `
-  //   default-src 'none';
-  //   base-uri 'none';
-  //   connect-src 'self' https://www.google-analytics.com/g/collect;
-  //   font-src 'self' ${gtmPreviewAndDebugSources ? "https://fonts.gstatic.com" : ""};
-  //   frame-src https://www.google.com/recaptcha/;
-  //   img-src 'self' data: https://lh3.googleusercontent.com ${gtmPreviewAndDebugSources ? "https://www.googletagmanager.com https://fonts.gstatic.com" : ""};
-  //   script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${env.NODE_ENV === "development" ? "'unsafe-eval'" : ""};
-  //   style-src 'self' 'nonce-${nonce}';
-  //   style-src-elem 'self' 'unsafe-inline' ${gtmPreviewAndDebugSources ? "https://www.googletagmanager.com https://fonts.googleapis.com" : ""};
-  //   upgrade-insecure-requests;
-  //   report-uri /api/csp-reports;
-  // `
+  switch (request.nextUrl.pathname) {
+    // Public whitelist for cached SEO crawlable pages.
+    case "/":
+    case "/privacy":
+    case "/login":
+    case "/signup":
+      // Allow Google Tag Manager preview and debug sources in development
+      const gtmPreviewAndDebugSources = env.NODE_ENV === "development"
+
+      // When NODE_ENV is "development", allow 'unsafe-eval' for webpack HMR
+      cspHeader = `
+        default-src 'none';
+        base-uri 'none';
+        connect-src 'self' https://www.google-analytics.com/g/collect;
+        font-src 'self' ${gtmPreviewAndDebugSources ? "https://fonts.gstatic.com" : ""};
+        frame-src https://www.google.com/recaptcha/;
+        img-src 'self' data: ${gtmPreviewAndDebugSources ? "https://www.googletagmanager.com https://fonts.gstatic.com" : ""};
+        script-src 'self' ${env.NODE_ENV === "development" ? "'unsafe-eval'" : ""};
+        script-src-elem 'self' 'unsafe-inline' https://www.googletagmanager.com/gtm.js https://www.googletagmanager.com/gtag/js https://www.google.com/recaptcha/enterprise.js https://www.gstatic.com/recaptcha/releases/;
+        style-src 'self';
+        style-src-elem 'self' 'unsafe-inline' ${gtmPreviewAndDebugSources ? "https://www.googletagmanager.com https://fonts.googleapis.com" : ""};
+        upgrade-insecure-requests;
+        report-uri /api/csp-reports;
+      `
+      break
+    default:
+      // Protected application pages with a nonce-based CSP header, where security is critical.
+      // WARNING: Using CSP header 'nonce' will prevent index/page caching as each request will have a unique nonce.
+      /* Docs:
+        - https://lh3.googleusercontent.com - Google auth user profile images
+        - https://fonts.gstatic.com/s/figtree/v5/ - TODO: Figure out why this is loaded so you can remove it from the CSP header.
+      */
+      const nonce = Buffer.from(crypto.randomUUID()).toString("base64")
+      cspHeader = `
+        default-src 'none';
+        base-uri 'none';
+        connect-src 'self';
+        font-src 'self' https://fonts.gstatic.com/s/figtree/v5/;
+        img-src 'self' data: https://lh3.googleusercontent.com;
+        script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${env.NODE_ENV === "development" ? "'unsafe-eval'" : ""};
+        style-src 'self' 'nonce-${nonce}';
+        style-src-elem 'self' 'unsafe-inline';
+        upgrade-insecure-requests;
+        report-uri /api/csp-reports;
+      `
+      requestHeaders.set("x-nonce", nonce)
+      break
+  }
 
   // Replace newline characters and spaces
   const contentSecurityPolicyHeaderValue = cspHeader
     .replace(/\s{2,}/g, " ")
     .trim()
 
-  const requestHeaders = new Headers(request.headers)
-  // requestHeaders.set("x-nonce", nonce)
   requestHeaders.set(
     "Content-Security-Policy",
     contentSecurityPolicyHeaderValue,

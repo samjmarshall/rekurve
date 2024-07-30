6d2b7e434b0fe1c86626c7cc9e4896b5ef1066a2
Sam Marshall <sam.marshall@v2.digital>
2024-07-30 16:37:25 +1000

    fix(progress): nullish operator

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import * as React from "react"
 import * as ProgressPrimitive from "@radix-ui/react-progress"
+import * as React from "react"
 
 import { cn } from "~/lib/utils"
 
@@ -13,13 +13,13 @@ const Progress = React.forwardRef<
     ref={ref}
     className={cn(
       "relative h-4 w-full overflow-hidden rounded-full bg-secondary",
-      className
+      className,
     )}
     {...props}
   >
     <ProgressPrimitive.Indicator
       className="h-full w-full flex-1 bg-primary transition-all"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      style={{ transform: `translateX(-${100 - (value ?? 0)}%)` }}
     />
   </ProgressPrimitive.Root>
 ))
